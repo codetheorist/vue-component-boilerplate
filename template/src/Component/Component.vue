7e2fcac73d0dfbe9c8a03a49cc19f910f7900eaf
Alex Scott <codetheorist@gmail.com>
2018-02-26 13:00:40 +0000

    Add dependency on to-pascal-case

---
+++ template/src/Component/Component.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <strong>\{{ text }}</strong>
+</template>
+
+<script>
+/**
+ * Component documentation
+ */
+export default {
+  name: '{{ name }}',
+  props: {
+    /**
+     * The component text
+     */
+    text: {
+      type: String,
+      default: '',
+      required: true
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped>
+  /* place here your styles */
+</style>
